511c404dfc924e5f2b510bcd901f77bb428924ba
guoyang-weng <140241253+guoyang-weng@users.noreply.github.com>
2023-08-03 23:19:00 +0200

    Reference added

--- src/mbf/tasks/__init__.py
+++ src/mbf/tasks/__init__.py
@@ -1,3 +1,6 @@
+# This file is a modified version of the file /src/tot/tasks/__init__.py from the repository https://github.com/princeton-nlp/tree-of-thought-llm.
+# The original file was created by the authors of the paper "Tree of Thoughts: Deliberate Problem Solving with Large Language Models", arxiv.org/abs/2305.10601
+
 def get_task(name):
     if name == 'game24':
         from mbf.tasks.game24 import Game24Task
