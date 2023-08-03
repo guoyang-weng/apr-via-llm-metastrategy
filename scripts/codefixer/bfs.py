a149c3612f317a26e1f5c25d1f732503b7b89658
guoyang-weng <140241253+guoyang-weng@users.noreply.github.com>
2023-08-03 02:12:52 +0200

    Script to execute and generate log file

---
+++ scripts/codefixer/bfs.py
@@ -0,0 +1,28 @@
+import subprocess
+
+command = [
+    "py",
+    "run.py",
+    "--task",
+    "codefixer",
+    "--task_start_index",
+    "0",
+    "--task_end_index",
+    "3",
+    "--prompt_sample",
+    "cot",
+    "--method_generate",
+    "sample",
+    "--method_evaluate",
+    "vote",
+    "--method_select",
+    "greedy",
+    "--n_generate_sample",
+    "5",
+    "--n_evaluate_sample",
+    "5",
+    "--n_select_sample",
+    "1"
+]
+
+subprocess.run(command)
