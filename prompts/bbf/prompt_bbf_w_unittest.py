5b9bb5b1a61dd79c82b07ae176625ead74df6b5b
guoyang-weng <140241253+guoyang-weng@users.noreply.github.com>
2023-08-04 13:02:07 +0200

    Create prompt_bbf_w_unittest.py

---
+++ prompts/bbf/prompt_bbf_w_unittest.py
@@ -0,0 +1,7 @@
+bbf_prompt_w_unittest = '''
+You will be given an input. The input, after the line "Buggy code:", contains a snippet of buggy Python code with a bug on a single line, and maybe unit test information. Given these inputs, output the fixed code after the line "Fixed code:".
+
+Buggy code:
+    {buggy_code}
+Fixed code:
+'''
